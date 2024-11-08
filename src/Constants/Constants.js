20f38d129bf0813c8cf4bcba6e31b35d88c6bd7f
AatiqR <aatiq9527@gmail.com>
2024-11-08 15:39:18 +0500

    first commit

---
+++ src/Constants/Constants.js
@@ -0,0 +1,388 @@
+import { FaBootstrap, FaCss3Alt, FaGithub, FaHtml5, FaJava, FaLinkedinIn, FaNodeJs, FaPython } from "react-icons/fa";
+import { IoLogoFirebase, IoLogoJavascript } from "react-icons/io5";
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandCpp, TbBrandNextjs } from "react-icons/tb";
+
+// for project 1
+import project1Img1 from '../assets/Project1/1.png'
+import project1Img2 from '../assets/Project1/2.png'
+import project1Img3 from '../assets/Project1/3.png'
+import project1Img4 from '../assets/Project1/4.png'
+import project1Img5 from '../assets/Project1/5.png'
+import project1Img6 from '../assets/Project1/6.png'
+import project1Img7 from '../assets/Project1/7.png'
+import project1Img8 from '../assets/Project1/8.png'
+
+// for project 2
+import project2Img1 from '../assets/Project2/1.png'
+import project2Img2 from '../assets/Project2/2.png'
+import project2Img3 from '../assets/Project2/3.png'
+import project2Img4 from '../assets/Project2/4.png'
+import project2Img5 from '../assets/Project2/5.png'
+import project2Img6 from '../assets/Project2/6.png'
+import project2Img7 from '../assets/Project2/7.png'
+
+// for project 3
+import project3Img1 from '../assets/Project3/1.png'
+import project3Img2 from '../assets/Project3/2.png'
+import project3Img3 from '../assets/Project3/3.png'
+import project3Img4 from '../assets/Project3/4.png'
+import project3Img5 from '../assets/Project3/5.png'
+
+// for project 4
+import project4Img1 from '../assets/Project4/1.png'
+import project4Img2 from '../assets/Project4/2.png'
+import project4Img3 from '../assets/Project4/3.png'
+import project4Img4 from '../assets/Project4/4.png'
+
+// for project 5
+import project5Img1 from '../assets/Project5/1.png'
+import project5Img2 from '../assets/Project5/2.png'
+import project5Img3 from '../assets/Project5/3.png'
+import project5Img4 from '../assets/Project5/4.png'
+import project5Img5 from '../assets/Project5/5.png'
+
+
+// for project 5
+import project6Img1 from '../assets/Project6/1.png'
+import project6Img2 from '../assets/Project6/2.png'
+import project6Img3 from '../assets/Project6/3.png'
+import project6Img4 from '../assets/Project6/4.png'
+import project6Img5 from '../assets/Project6/5.png'
+import project6Img6 from '../assets/Project6/6.png'
+import project6Img7 from '../assets/Project6/7.png'
+import project6Img8 from '../assets/Project6/8.png'
+import project6Img9 from '../assets/Project6/9.png'
+import project6Img10 from '../assets/Project6/10.png'
+import project6Img11 from '../assets/Project6/11.png'
+import project6Img12 from '../assets/Project6/12.png'
+import project6Img13 from '../assets/Project6/13.png'
+import project6Img14 from '../assets/Project6/14.png'
+import project6Img15 from '../assets/Project6/15.png'
+import project6Img16 from '../assets/Project6/16.png'
+import project6Img17 from '../assets/Project6/17.png'
+import project6Img18 from '../assets/Project6/18.png'
+import project6Img19 from '../assets/Project6/19.png'
+import project6Img20 from '../assets/Project6/20.png'
+
+// socialIcons
+import { FaFacebookF } from "react-icons/fa";
+
+// lightModeImg
+import lightModeImg from '../assets/lightModeImg.png'
+import nightModeImg from '../assets/nightModeImg.png'
+
+// institutesImages
+import schoolLogo from '../assets/schoolLogo.png'
+import collegeLogo from '../assets/collegeLogo.png'
+import universityLogo from '../assets/universityLogo.png'
+import smitLogo from '../assets/smitLogo.png'
+import digiskillsLogo from '../assets/digiskillsLogo.png'
+
+// navbarLogo
+import navbarImg from '../assets/navbarLogo.png'
+import { SiAntdesign, SiDjango, SiExpress, SiMongodb, SiPostman, SiTypescript } from "react-icons/si";
+
+
+const skillsSubTitle = "Crazy full stack developer who wants to explore every tech stack";
+
+// name
+const name = "Mohammad Saad Aly";
+
+// navbarLogo
+const navbarLogo = navbarImg;
+
+// skillsIcons
+const skillsIcons = [
+    {
+        icon: <FaHtml5 />,
+        title: "Html"
+    },
+    {
+        icon: <FaCss3Alt />,
+        title: "CSS"
+    },
+    {
+        icon: <FaBootstrap />,
+        title: "Bootstrap"
+    },
+    {
+        icon: <IoLogoJavascript />,
+        title: "JavaScript"
+    },
+    {
+        icon: <SiTypescript />,
+        title: "TypeScript"
+    },
+    {
+        icon: <IoLogoFirebase />,
+        title: "Firebase"
+    },
+    {
+        icon: <RiReactjsLine />,
+        title: "React"
+    },
+    {
+        icon: <TbBrandNextjs />,
+        title: "Next.JS"
+    },
+    {
+        icon: <SiMongodb />,
+        title: "MongoDB"
+    },
+    {
+        icon: <SiExpress />,
+        title: "Express.JS"
+    },
+    {
+        icon: <FaNodeJs />,
+        title: "Node.JS"
+    },
+    {
+        icon: <SiAntdesign />,
+        title: "Ant Design"
+    },
+    {
+        icon: <SiPostman />,
+        title: "Postman"
+    },
+    {
+        icon: <FaPython />,
+        title: "Python"
+    },
+    {
+        icon: <SiDjango />,
+        title: "Django"
+    },
+    {
+        icon: <FaJava />,
+        title: "Java"
+    },
+    {
+        icon: <TbBrandCpp />,
+        title: "C++"
+    },
+];
+
+// education
+const education = [
+    {
+        "img": universityLogo,
+        "instituteName": "Sindh Madressatul Islam University",
+        "degreeName": "Bachelor in Computer Science",
+        "duration": "2020 - 2024"
+    },
+    
+    {
+        "img": collegeLogo,
+        "instituteName": "Govt Delhi College",
+        "degreeName": "Intermediate (Medical Regular)",
+        "duration": "2016 - 2018"
+    },
+    
+    {
+        img: schoolLogo,
+        instituteName: "Mumtaz Vision Academy",
+        degreeName: "Matriculation in Science Group",
+        duration: "March 2019 - October 2021"
+    }
+]
+
+// resumeLink
+const resume = "https://drive.google.com/file/d/14jAXKSOYswT5gFusR6aUtxJu54ASjNHw/view"
+
+// professionalExperience
+const professionalExperience = [
+    {
+        img: smitLogo,
+        name: "Saylani Mass IT Training Program",
+        role: "Lead Instructor at SMIT  teaching web and mobile app Development",
+        duration: "2021 - 2024 (3 Year)",
+        certified: false
+    },
+    {
+        img: smitLogo,
+        name: "Saylani Mass IT Training Program",
+        role: "Web & Mobile App Development Course",
+        duration: "June 2023 -  June 2024 (1 Year)",
+        certified: true
+    },
+    {
+        img: smitLogo,
+        name: "Saylani Mass IT Training Program",
+        role: "Programming Essentials in Python",
+        duration: "December 2021 - March 2022 (3 Months)",
+        certified: true
+    },
+    {
+        img: digiskillsLogo,
+        name: "DigiSkills",
+        role: "COMMUNICATION AND SOFT SKILLS",
+        duration: "July 2022 - October 2022 (3 Months)",
+        certified: true
+    },
+]
+
+// skillsList
+const skillsList = [
+    "Develop highly interactive Front end / User Interfaces for your websites.",
+    "Integration of third party services such as Firebase.",
+    "Easily create responsive sites."
+]
+
+// servicesList
+const servicesList = [
+    {
+        title: "Website Development",
+        description: "Create responsive and interactive websites for individuals, businesses, or organizations using your skills in HTML5, CSS3, JavaScript, and Bootstrap. Ensure that the websites are mobile-friendly and optimized for various devices and screen sizes."
+    },
+    {
+        title: "Frontend Development",
+        description: "Specialize in frontend development by building user interfaces (UIs) and user experiences (UX) using HTML5, CSS3, JavaScript, and React.js. Develop single-page applications (SPAs) and ensure they are fast, responsive, and accessible."
+    },
+    {
+        title: "Custom Web Applications",
+        description: "Utilize React.js along with backend technologies (like Node.js, Express.js, or others) to develop custom web applications. Use Firebase for real-time data syncing and backend services, such as authentication, databases, and hosting."
+    },
+    {
+        title: "Web Hosting and Deployment",
+        description: "Assist clients in deploying their websites and web applications to hosting platforms such as Firebase Hosting, Netlify, or traditional web servers. Optimize website performance and ensure secure connections (HTTPS)."
+    },
+    {
+        title: "Maintenance and Updates",
+        description: "Offer maintenance services to keep websites and web applications up-to-date with security patches, performance improvements, and feature enhancements. Provide regular backups and monitoring for uptime."
+    },
+    {
+        title: "Responsive Design Audits",
+        description: "Evaluate existing websites for responsiveness across devices and browsers. Provide recommendations and implement improvements using CSS3 media queries and responsive design principles."
+    },
+    {
+        title: "Integration with APIs",
+        description: "Integrate third-party APIs (such as social media APIs, or data APIs) into web applications using JavaScript and frameworks like React.js. Ensure secure and efficient data exchange between systems."
+    },
+]
+
+// portfolio
+const portfolio = [
+    {
+        images: [
+            project6Img1,
+            project6Img2,
+            project6Img3,
+            project6Img4,
+            project6Img5,
+            project6Img6,
+            project6Img7,
+            project6Img8,
+            project6Img9,
+            project6Img10,
+            project6Img11,
+            project6Img12,
+            project6Img13,
+            project6Img14,
+            project6Img15,
+            project6Img16,
+            project6Img17,
+            project6Img18,
+            project6Img19,
+            project6Img20,
+        ],
+        title: "Assignment Submission Portal",
+        description: "A web-based platform designed for streamlined assignment submissions and management. Teachers can create and manage assignments, while students can submit their work with ease. Features include user authentication, real-time updates, and a responsive interface. Built with MERN stack.",
+        link: "https://assignment-submission-portal.vercel.app/"
+    },
+    {
+        images: [
+            project1Img1,
+            project1Img2,
+            project1Img3,
+            project1Img4,
+            project1Img5,
+            project1Img6,
+            project1Img7,
+            project1Img8,
+        ],
+        title: "RestaurantHub",
+        description: "Streamlined restaurant website with easy navigation. Explore restaurants and menus, sign in to securely add items to your cart. Admins enjoy intuitive management, adding restaurants, and CRUD operations on menu items. Elevate your dining experience with RestaurantHub.",
+        link: "https://restauranthub.vercel.app/"
+    },
+    {
+        images: [
+            project2Img1,
+            project2Img2,
+            project2Img3,
+            project2Img4,
+            project2Img5,
+            project2Img6,
+            project2Img7,
+        ],
+        title: "Dentist Website",
+        description: "I have created a dentist website using React and Firebase, where users can perform CRUD operations on patient data after logging in. The website also provides daily and monthly data views. Additionally, users can manage pre-received amounts of patients through CRUD operations.",
+        link: "https://react-dentist-website.vercel.app/"
+    },
+    {
+        images: [
+            project3Img1,
+            project3Img2,
+            project3Img3,
+            project3Img4,
+            project3Img5,
+        ],
+        title: "Blogging Website",
+        description: "I've built a responsive blog website with JavaScript for seamless data operations using Firebase. Security is a top priority, leveraging Firebase features to protect user data. The focus is on delivering a precise, secure, and user-friendly blogging experience.",
+        link: "https://blogging-website-huzaifa.vercel.app/"
+    },
+    {
+        images: [
+            project4Img1,
+            project4Img2,
+            project4Img3,
+            project4Img4,
+        ],
+        title: "Todo Website",
+        description: "I have developed a fully responsive todo website that efficiently stores data for each user, and authentication is implemented using Firebase, ensuring secure access to user accounts. Users can perform CRUD operations on todos within the website, enhancing productivity within the platform.",
+        link: "https://firebase-todo-huzaifa.vercel.app/"
+    },
+    {
+        images: [
+            project5Img1,
+            project5Img2,
+            project5Img3,
+            project5Img4,
+            project5Img5,
+        ],
+        title: "Web Agency",
+        description: "This website depicts a web agency's homepage. The website highlights the agency’s services, which include refreshing design, speed optimization, and full customization. The website also features testimonials from satisfied clients and a call to action to download the agency’s app.",
+        link: "https://next-agency-web.vercel.app/"
+    },
+]
+
+// contact
+const contact = {
+    number: "+92 3242513101",
+    email: "mohammadsaadali786.92@gmail.com",
+    location: "Garden West Karachi"
+}
+
+// socialLinks
+const socialLinks = [
+    {
+        icon: <FaFacebookF />,
+        link: "https://www.facebook.com/profile.php?id=100090157283290"
+    },
+    {
+        icon: <FaGithub />,
+        link: "https://github.com/Huzaifa-Khan-Official"
+    },
+    {
+        icon: <FaLinkedinIn />,
+        link: "https://www.linkedin.com/in/huzaifa-khan-officia/"
+    }
+]
+
+const themes = {
+    lightModeImg,
+    nightModeImg
+}
+
+export { skillsSubTitle, skillsIcons, skillsList, servicesList, portfolio, contact, socialLinks, name, themes, education, professionalExperience, resume, navbarLogo }
